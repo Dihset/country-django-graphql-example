cb82761d2e51d747b7e5e98db78b9b8d7a9d3fd6
Slamihin Egor <slamihin123@gmail.com>
2021-05-19 11:48:27 +0400

    city subscribtion

--- project/graphql/signals.py
+++ project/graphql/signals.py
@@ -1,11 +1,16 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from project.country.models import Country
+from project.country.models import City, Country
 
-from .subscription import OnNewCountry
+from .subscription import OnNewCity, OnNewCountry
 
 
 @receiver(post_save, sender=Country)
 def broadcast_country(sender, instance, **kwargs):
     OnNewCountry.new_country(instance)
+
+
+@receiver(post_save, sender=City)
+def broadcast_city(sender, instance, **kwargs):
+    OnNewCity.new_city(instance)
--- project/graphql/subscription.py
+++ project/graphql/subscription.py
@@ -1,9 +1,11 @@
 import channels_graphql_ws
 import graphene
 
+from project.country.models import City as CityModel
 from project.country.models import Country as CountryModel
 
-from .query import CountryType
+from .query import CityType, CountryType
+from .utils import CustomNode
 
 
 class OnNewCountry(channels_graphql_ws.Subscription):
@@ -21,7 +23,32 @@ class OnNewCountry(channels_graphql_ws.Subscription):
         )
 
 
+class OnNewCity(channels_graphql_ws.Subscription):
+    """Subscription triggers on a new city."""
+
+    class Arguments:
+        country_id = graphene.String(required=True)
+
+    city = graphene.Field(CityType)
+
+    def publish(self, info, country_id, **kwargs):
+        _type, _country_id = CustomNode.from_global_id(country_id)
+        if _type != CountryType.__name__:
+            raise Exception("some error")
+        city = self.get("city")
+        if city.country_id != int(_country_id):
+            return OnNewCity.SKIP
+        return OnNewCity(city=city)
+
+    @classmethod
+    def new_city(cls, city: CityModel):
+        cls.broadcast(
+            payload={"city": city},
+        )
+
+
 class Subscription(graphene.ObjectType):
     """GraphQL subscriptions."""
 
     on_new_country = OnNewCountry.Field()
+    on_new_city = OnNewCity.Field()
