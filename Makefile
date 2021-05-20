2f1f41e3490399872415b47c5acafd63321cadd0
Slamihin Egor <slamihin123@gmail.com>
2021-05-20 10:14:05 +0400

    complite query tests

--- Makefile
+++ Makefile
@@ -4,21 +4,23 @@ DC=docker-compose
 .PHONY: create-network
 create-network:
 	docker network create country-django-graphql-example-network
-	
+
+
 .PHONY: up-storages
 up-storages:
 	${DC} -f docker-compose/storages.yml up -d
+
 
 .PHONY: setup-dev
 setup-dev:
 	./manage.py runserver 0.0.0.0:8000
 
+
 .PHONY: testing
 testing:
 	pytest --cov-report xml:cov.xml --cov .
-	
 
-#TODO isort + black
+
 .PHONY: linting
 linting:
 	autopep8 --aggressive --experimental -r -i ./project
--- project/graphql/tests/test_queries.py
+++ project/graphql/tests/test_queries.py
@@ -49,3 +49,51 @@ def test_get_one_city(client_query, city_1):
     assert content["data"]["city"] == factory_to_dict(
         city_1, exclude=["id", "country_id"]
     )
+
+
+@pytest.mark.usefixtures("client_query")
+@pytest.mark.django_db
+def test_get_many_cities(client_query, city_1, city_2, city_3, city_4):
+    query = """{
+      allCities {
+        edges {
+          node {
+            name
+            population
+          }
+        }
+      }
+    }
+    """
+    response = client_query(query)
+    content = response.json()
+    expected = [
+        factory_to_dict(city, exclude=["id", "country_id"])
+        for city in [city_1, city_2, city_3, city_4]
+    ]
+    received = [city["node"] for city in content["data"]["allCities"]["edges"]]
+    assert all((city in received for city in expected))
+
+
+@pytest.mark.usefixtures("client_query")
+@pytest.mark.django_db
+def test_get_many_countries(client_query, country_1, country_2, country_3, country_4):
+    query = """{
+      allCountries {
+        edges {
+          node {
+            name
+            population
+          }
+        }
+      }
+    }
+    """
+    response = client_query(query)
+    content = response.json()
+    expected = [
+        factory_to_dict(country, exclude=["id"])
+        for country in [country_1, country_2, country_3, country_4]
+    ]
+    received = [country["node"] for country in content["data"]["allCountries"]["edges"]]
+    assert all((country in received for country in expected))
